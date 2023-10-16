64225a5ee66085ad9fbb0dfc185457d66833d122
aistiak <aristiak1994@gmail.com>
2023-10-16 13:59:36 +0600

    pdf generator added

--- app/backend/src/routes/index.routes.ts
+++ app/backend/src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import ValuationRoutes from './valuation.routes';
 import RiskRouter from './risk.routes';
+import ReportRouter from './report.routes';
 
 const RootRoutes = Router();
 
 RootRoutes.use('/', ValuationRoutes);
 RootRoutes.use('/',RiskRouter) ;
+RootRoutes.use('/',ReportRouter);
 
 export default RootRoutes;
---
+++ app/backend/src/routes/report.routes.ts
@@ -0,0 +1,64 @@
+import { Router } from 'express';
+
+
+const ReportRouter = Router();
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const base = '/report';
+
+
+ReportRouter.post(`${base}`, async (req, res, next) => {
+    try {
+
+
+        const browser = await puppeteer.launch();
+
+        // Create a new page
+        const page = await browser.newPage();
+
+        // Website URL to export as pdf
+        const website_url = req.body.req_page;
+        await page.goto(website_url, { waitUntil: 'networkidle0', timeout: 0 });
+        await page.emulateMediaType('screen');
+        const pdf = await page.pdf({
+            headerTemplate: '<span style="font-size: 30px; width: 200px; height: 200px; background-color: black; color: white; margin: 20px;">Header</span>',
+            fofooterTemplate: "<div><div class='pageNumber'></div> <div>/</div><div class='totalPages'></div></div>",
+
+            displayHeaderFooter: true,
+
+            path: 'result.pdf',
+            margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+            printBackground: true,
+            format: 'A4',
+        });
+
+
+        // Close the browser instance
+        await browser.close();
+
+        var file = fs.createReadStream('./result.pdf');
+        var stat = fs.statSync('./result.pdf');
+        res.setHeader('Content-Length', stat.size);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=cost-calculator.pdf');
+        file.pipe(res);
+
+        res.on('finish', function () {
+            // The response has been sent completely
+            // You can now safely delete the file
+            fs.unlink('./result.pdf', (err) => {
+                if (err) {
+                    console.error('Error deleting the file:', err);
+                } else {
+                    console.log('File deleted successfully');
+                }
+            });
+        });
+
+    } catch (e) {
+        next(e)
+    }
+})
+
+export default ReportRouter ;
